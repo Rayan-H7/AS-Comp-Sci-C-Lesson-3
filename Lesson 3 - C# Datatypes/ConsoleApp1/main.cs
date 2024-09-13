d08c581e1c3e8ef0f1807222a092b0bdb2114556
Rayan Hassan <98430041+Rayan-H7@users.noreply.github.com>
2024-09-13 12:48:43 +0100

    Add files via upload

---
+++ Lesson 3 - C# Datatypes/ConsoleApp1/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataTypes{
+    class Program{
+        static void Prog(string[] args){
+            Console.WriteLine("Enter the radius in m: ");
+            string radios = Console.ReadLine();
+            float Pi = 3.14f;
+            double radius =  Convert.ToDouble(radios);
+            double circumference =  radius * Pi * 2;
+            Console.WriteLine("The circumference is " + circumference + "m \n");
+            double area = radius * radius * Pi;
+            Console.WriteLine("The area is " + area + "m^2");
+        }
+
+        static void Main(string[] args){
+            Console.WriteLine("Enter your mark out of 60");
+            double Mark = Convert.ToDouble(Console.ReadLine());
+            double percentage = (Mark / 60) * 100;
+            Console.WriteLine("Your percentage in the test is " + percentage + "% \n");
+        }
+    }
+}
